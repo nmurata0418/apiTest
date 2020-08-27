c8ab9ec4b309f64e25c1b0469f9e3ac69b53ca2a
nmurata0418 <nmurata@skywill.jp>
2020-08-27 18:00:20 +0900

    修正版

--- api-practice/src/App.vue
+++ api-practice/src/App.vue
@@ -6,15 +6,19 @@
       :key="post.id"
     >
       <br>
+      <!-- テーブル Start -->
       <div class="data">
         <table>
           <tbody>
+            <!-- id表示 -->
             <tr class="data-id">
               {{post.id}}
             </tr>
+            <!-- title表示 -->
             <tr class="data-title">
               {{post.title}}
             </tr>
+            <!-- completed表示、trueなら完了、falseは未完了 -->
             <tr class="data-completed" v-if='post.completed === true'>
               完了
             </tr>
@@ -24,6 +28,7 @@
           </tbody>
         </table>
       </div>
+      <!-- テーブル End -->
     </div>
   </div>
 </template>
--- api-practice/src/axios-auth.js
+++ api-practice/src/axios-auth.js
@@ -1,8 +1,6 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL:
-    'https://jsonplaceholder.typicode.com/todos'
 });
 
 export default instance;
